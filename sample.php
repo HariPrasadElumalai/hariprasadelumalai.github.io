25c2116bc0f5bdf6a2aa597264c14a12ad4a3d24
HariPrasadElumalai <hariprasad.e97@gmail.com>
2016-03-20 11:08:01 +0530

    Added files via upload

---
+++ sample.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<p>&lt;?php</p>
+<p><strong>echo</strong> &quot;HELLO TO ALL&quot;;</p>
+<p><strong>?&gt;</strong></p>
+</body>
+</html>
